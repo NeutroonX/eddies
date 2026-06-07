9d8e0b0b6cb5e61f97f548b0f222738058a390a0
neutron <kranthicodes4@gmail.com>
2026-06-07 17:56:08 +0530

    feat: replace app icons and logos with new brand assets

---
+++ src/constants/images.ts
@@ -0,0 +1,5 @@
+export const EddiesImages = {
+  logo: require('../../assets/images/logo.png') as number,
+  logoSvg: require('../../assets/images/logo.svg') as number,
+  logoNoBg: require('../../assets/images/logo_no_bg.png') as number,
+} as const;
